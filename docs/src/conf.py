ea9723d1a47fd4b723ce3eae3d63349dc4570940
Matthias Kellner <bananenpampe@t-online.de>
2024-11-18 14:07:46 +0100

    remove rascaline doc link

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -30,7 +30,6 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "torch": ("https://pytorch.org/docs/stable/", None),
-    "rascaline": ("https://luthaf.fr/rascaline/latest/", None),
     "metatensor": ("https://docs.metatensor.org/latest/", None),
     "ase": ("https://wiki.fysik.dtu.dk/ase/", None),
 }
