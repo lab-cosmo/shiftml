c6368dba7d9758691522c875268a143a335ead98
Matthias Kellner <bananenpampe@t-online.de>
2025-06-14 23:46:14 +0200

    move to sml3

--- src/shiftml/ase/calculator.py
+++ src/shiftml/ase/calculator.py
@@ -1,67 +1,41 @@
 import logging
 import os
 import urllib.request
-import numpy as np
 
+import numpy as np
 from metatensor.torch.atomistic import ModelOutput
 from metatensor.torch.atomistic.ase_calculator import MetatensorCalculator
 from platformdirs import user_cache_path
 
 from shiftml.utils.tensorial import T_sym_np_inv, symmetrize
 
-# For now we set the logging level to DEBUG
+# For now we set the logging level to INFO
 logformat = "%(asctime)s - %(levelname)s - %(message)s"
-logging.basicConfig(level=logging.DEBUG, format=logformat)
+logging.basicConfig(level=logging.INFO, format=logformat)
 
 
-url_resolve = {
-    "ShiftML1.1rev": "https://tinyurl.com/msnss4ds",
-    "ShiftML2.0rev": "https://tinyurl.com/3axupmsd",
-    "ShiftML2.1dev": "https://zenodo.org/record/14920547/files/model.pt?download=1",
-    "ShiftML2.1dev_csa": "https://zenodo.org/records/14962123/files/model_csa.pt?download=1",
-}
+url_resolve = {}
 
 cs_iso_output = {"mtt::cs_iso": ModelOutput(quantity="", unit="ppm", per_atom=True)}
 
-cs_iso_ensemble_output = {
-        "mtt::cs_iso": ModelOutput(quantity="", unit="ppm", per_atom=True),
-        "mtt::cs_iso_std": ModelOutput(quantity="", unit="ppm", per_atom=True),
-        "mtt::cs_iso_ensemble": ModelOutput(quantity="", unit="ppm", per_atom=True),
-    },
-
 resolve_outputs = {
-    "ShiftML1.1rev": cs_iso_output,
-    "ShiftML2.1dev": cs_iso_output,
-    "ShiftML2.0rev": cs_iso_ensemble_output,
-    "ShiftML2.1dev_ensemble": cs_iso_output,
-    "ShiftML2.1csa_dev_ensemble": cs_iso_output,
-    "ShiftML2.1dev_csa": cs_iso_output,
+    "ShiftML3": cs_iso_output,
 }
 
 resolve_fitted_species = {
-    "ShiftML1.1rev": set([1, 6, 7, 8, 16]),
-    "ShiftML2.0rev": set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20]),
-    "ShiftML2.1dev": set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20]),
-    "ShiftML2.1dev_ensemble": set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20]),
-    "ShiftML2.1csa_dev_ensemble": set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20]),
-    "ShiftML2.1dev_csa": set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20]),
+    "ShiftML3": set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20]),
 }
 
-requires_metatrain = ["ShiftML2.1dev_ensemble", "ShiftML2.1dev_csa"]
-
-# prepares ensemble model
-for i in range(1,8):
-    url_resolve["ShiftML2.1dev" + str(i)] = f"https://zenodo.org/records/14920832/files/model_{i}.pt?download=1"
-    resolve_fitted_species["ShiftML2.1dev" + str(i)] = set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20])
-    resolve_outputs["ShiftML2.1dev" + str(i) ] = cs_iso_output
-    requires_metatrain.append("ShiftML2.1dev" + str(i))
-
 # prepares cs_ensemble model
-for i in range(0,8):
-    url_resolve["ShiftML2.1csa_dev" + str(i)] = f"https://zenodo.org/records/15079415/files/model_{i}.pt?download=1"
-    resolve_fitted_species["ShiftML2.1csa_dev" + str(i)] = set([1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20])
-    resolve_outputs["ShiftML2.1csa_dev" + str(i)] = cs_iso_output
-    requires_metatrain.append("ShiftML2.1csa_dev" + str(i))
+for i in range(0, 8):
+    url_resolve["ShiftML3" + str(i)] = (
+        f"https://zenodo.org/records/15079415/files/model_{i}.pt?download=1"
+    )
+    resolve_fitted_species["ShiftML3" + str(i)] = set(
+        [1, 6, 7, 8, 9, 11, 12, 15, 16, 17, 19, 20]
+    )
+    resolve_outputs["ShiftML3" + str(i)] = cs_iso_output
+
 
 def is_fitted_on(atoms, fitted_species):
     if not set(atoms.get_atomic_numbers()).issubset(fitted_species):
@@ -72,39 +46,45 @@ def is_fitted_on(atoms, fitted_species):
             with only the fitted species."
         )
 
-def ShiftML(model_version, force_download=False):
-        """
-        Initialize the ShiftML calculator
 
-        Parameters
-        ----------
-        model_version : str
-            The version of the ShiftML model to use. Supported versions are
-            "ShiftML1.1rev" and "ShiftML2.0rev" "ShiftML2.1devel".
-        force_download : bool, optional
-            If True, the model will be downloaded even if it is already in the cache.
-            The chache-dir will be determined via the platformdirs library and should
-            comply with user settings such as XDG_CACHE_HOME.
-            Default is False.
-        """
+def ShiftML(model_version, force_download=False, device=None):
+    """
+    Initialize the ShiftML calculator
 
-        # its not perfect, it is what it is...
-        if model_version in ["ShiftML2.1dev_ensemble", "ShiftML2.1csa_dev_ensemble"]:
-            model_version = model_version.replace("_ensemble", "")
-            model_list = []
-            
-            if model_version == "ShiftML2.1csa_dev":
-                for i in range(0,8):
-                    model_list.append(ShiftML_model(model_version + str(i), force_download=force_download))
-            elif model_version == "ShiftML2.1dev":
-                for i in range(1,8):
-                    model_list.append(ShiftML_model(model_version + str(i), force_download=force_download))
-            
-            return ShiftML_ensemble(model_list)
-        
-        else:
-            return  ShiftML_model(model_version, force_download=force_download)
+    Parameters
+    ----------
+    model_version : str
+        The version of the ShiftML model to use. Supported versions are
+        "ShiftML3"
+    force_download : bool, optional
+        If True, the model will be downloaded even if it is already in the cache.
+        The chache-dir will be determined via the platformdirs library and should
+        comply with user settings such as XDG_CACHE_HOME.
+        Default is False.
+    device : str, optional
+        The device to use for the model. If None, the preferred device will be saught
+        from the environment (e.g. CUDA if available), it will fall back to CPU if no
+        preffered device is found.
+        If you want to use a specific device, you can set it to "cpu" or "cuda".
+        Default is None.
+    """
 
+    # its not perfect, it is what it is...
+    if model_version in ["ShiftML3"]:
+        model_list = []
+        for i in range(0, 8):
+            model_list.append(
+                ShiftML_model(
+                    model_version + str(i), force_download=force_download, device=device
+                )
+            )
+
+        return ShiftML_ensemble(model_list)
+
+    else:
+        return ShiftML_model(
+            model_version, force_download=force_download, device=device
+        )
 
 
 class ShiftML_ensemble:
@@ -114,104 +94,77 @@ class ShiftML_ensemble:
         """
         self.models = model_list
 
+    def get_cs_tensor_ensemble(self, atoms, return_symmetric=True):
+        cs_tensors = []
+
+        for model in self.models:
+            out = model.get_cs_tensor(atoms, return_symmetric=return_symmetric)
+            cs_tensors.append(out)
+
+        cs_tensors = np.stack(cs_tensors, axis=-1)
+
+        # last dimension is ensemble member shape
+        cs_tensors = cs_tensors.reshape(-1, 3, 3, cs_tensors.shape[-1])
+
+        return cs_tensors
+
+    def get_cs_iso_ensemble(self, atoms):
+
+        cs_tensors = self.get_cs_tensor_ensemble(atoms, return_symmetric=True)
+        cs_isos = np.trace(cs_tensors, axis1=1, axis2=2) / 3
+
+        return cs_isos
+
     def get_cs_iso(self, atoms):
         """
         Compute the shielding values for the given atoms object
         """
 
-        cs_isos = []
+        cs_isos = self.get_cs_iso_ensemble(atoms)
 
-        for model in self.models:
-            out = model.get_cs_iso(atoms)
-            cs_isos.append(out)
-
-        cs_iso = np.mean(np.hstack(cs_isos),axis=1)
+        cs_iso = np.mean(cs_isos, axis=-1)
 
         return cs_iso
 
-    def get_cs_iso_ensemble(self, atoms):
-        
-        cs_isos = []
+    def get_cs_tensor(self, atoms, return_symmetric=True):
+        """
+        Compute the shielding tensors for the given atoms object
+        """
 
-        for model in self.models:
-            out = model.get_cs_iso(atoms)
-            cs_isos.append(out)
+        cs_tensors = self.get_cs_tensor_ensemble(
+            atoms, return_symmetric=return_symmetric
+        )
 
-        cs_iso = np.hstack(cs_isos)
-
-        return cs_iso
-    
-    def get_cs_tensor_ensemble(self, atoms, return_symmetric=True):
-        cs_tensors = []
-        
-        for model in self.models:
-            out = model.get_cs_tensor(atoms, return_symmetric=return_symmetric)
-            cs_tensors.append(out)
-        
-        cs_tensors = np.stack(cs_tensors,axis=-1)
+        cs_tensors = np.mean(cs_tensors, axis=-1).reshape(-1, 3, 3)
 
         return cs_tensors
-    
+
+
 class ShiftML_model(MetatensorCalculator):
     """
     ShiftML calculator for ASE
     """
 
-    def __init__(self, model_version, force_download=False):
+    def __init__(self, model_version, force_download=False, device=None):
         """
         Initialize the ShiftML calculator
 
         Parameters
         ----------
         model_version : str
-            The version of the ShiftML model to use. Supported versions are
-            "ShiftML1.1rev" and "ShiftML2.0rev" "ShiftML2.1devel".
+            The version of the ShiftML model to use.
         force_download : bool, optional
             If True, the model will be downloaded even if it is already in the cache.
             The chache-dir will be determined via the platformdirs library and should
             comply with user settings such as XDG_CACHE_HOME.
             Default is False.
+        device : str, optional
+            The device to use for the model. If None, the preferred
+            device will be sought from the environment (e.g. CUDA if available),
+            it will fall back to CPU if no preferred device is found.
+            If you want to use a specific device, you can set it to "cpu" or "cuda".
+            Default is None.
         """
-
-        if model_version in ["ShiftML1.1rev", "ShiftML2.0rev"]:
-            try:
-                # The rascline import is necessary because
-                # it is required for the scripted model
-                import featomic.torch
-
-                logging.info("featomic version: {}".format(featomic.torch.__version__))
-                logging.info("featomic-torch is installed, importing featomic-torch")
-
-                assert (
-                    featomic.torch.__version__ == "0.1.0.dev0" #"0.1.0.dev597"
-                ), "wrong featomic-torch installed"
-
-            except ImportError:
-                raise ImportError(
-                    "featomic-torch is required for featomic based ShiftML calculators,\
-                    please install it using\
-                    pip install git+https://github.com/metatensor/featomic#subdirectory\
-                    =python/featomic-torch"
-                )
-        ### lol why doesnt this break?????
-
-        elif model_version in ["ShiftML2.1dev","ShiftML2.1dev_csa"]:
-            try: # 0.1.dev300+g7a465bf
-                import metatrain
-                logging.info("metatrain version: {}".format(metatrain.__version__))
-                logging.info("metatrain is installed, importing metatrain")
-
-                assert (
-                    metatrain.__version__ == "0.1.dev300+g7a465bf"
-                ), "wrong featomic-torch installed"
-            
-            except ImportError:
-                raise ImportError(
-                    "metatrain is required for nanoPET based ShiftML calculators,\
-                    please install it using\
-                    pip install metatrain[nanopet]@git+https://github.com/metatensor/\
-                    metatrain@7a465bf"
-                    )
 
         try:
             url = url_resolve[model_version]
@@ -294,48 +247,13 @@ class ShiftML_model(MetatensorCalculator):
             )
             raise e
 
-        super().__init__(model_file)
+        super().__init__(
+            model_file,
+            device=device,
+        )
+
         self.model_version = model_version
 
-    def get_cs_iso(self, atoms):
-        """
-        Compute the shielding values for the given atoms object
-        """
-        assert (
-            "mtt::cs_iso" in self.outputs.keys()
-        ), "model does not support chemical shielding prediction"
-
-        is_fitted_on(atoms, self.fitted_species)
-
-        out = self.run_model(atoms, self.outputs)
-        cs_iso = out["mtt::cs_iso"].block(0).values.detach().to("cpu").numpy()
-
-        return cs_iso
-
-    def get_cs_iso_std(self, atoms):
-        assert (
-            "mtt::cs_iso_std" in self.outputs.keys()
-        ), "model does not support chemical shielding prediction"
-
-        is_fitted_on(atoms, self.fitted_species)
-
-        out = self.run_model(atoms, self.outputs)
-        cs_iso_std = out["mtt::cs_iso_std"].block(0).values.detach().to("cpu").numpy()
-
-        return cs_iso_std
-
-    def get_cs_iso_ensemble(self, atoms):
-        assert (
-            "mtt::cs_iso_ensemble" in self.outputs.keys()
-        ), "model does not support chemical shielding prediction"
-
-        is_fitted_on(atoms, self.fitted_species)
-
-        out = self.run_model(atoms, self.outputs)
-        cs_iso_ensemble = out["mtt::cs_iso_ensemble"].block(0).values.detach().to("cpu").numpy()
-
-        return cs_iso_ensemble
-    
     def get_cs_tensor(self, atoms, return_symmetric=True):
         assert (
             "mtt::cs_iso" in self.outputs.keys()
@@ -344,11 +262,19 @@ class ShiftML_model(MetatensorCalculator):
         is_fitted_on(atoms, self.fitted_species)
 
         out = self.run_model(atoms, self.outputs)
-        out =  out["mtt::cs_iso"].components_to_properties(["o3_mu"])
 
-        pred_vals = np.concatenate([block.values.to("cpu").numpy() for block in out.blocks()],axis=1) @ T_sym_np_inv.T
+        # TODO: currently ShiftML3 predicts tensors with the label  "mtt::cs_iso"
+        # later this should be changed to "mtt::cs_tensor"
+        out = out["mtt::cs_iso"].components_to_properties(["o3_mu"])
+
+        pred_vals = (
+            np.concatenate(
+                [block.values.to("cpu").numpy() for block in out.blocks()], axis=1
+            )
+            @ T_sym_np_inv.T
+        )
 
         if return_symmetric:
             pred_vals = symmetrize(pred_vals)
 
-        return pred_vals 
+        return pred_vals
--- tests/test_ase.py
+++ tests/test_ase.py
@@ -6,205 +6,76 @@ from ase.build import bulk
 
 from shiftml.ase import ShiftML
 
-expected_output = np.array([137.5415, 137.5415])
+expected_outputs = {"ShiftML3": np.array([70.39079043, 70.4060931])}
 
-expected_ensemble_v2 = np.array(
-    [
+expected_outputs_tensors = {
+    "ShiftML3": np.array(
         [
-            114.8194808959961,
-            113.47244262695312,
-            117.47064208984375,
-            115.61190795898438,
-            130.88909912109375,
-            131.42332458496094,
-            120.96844482421875,
-            115.95867919921875,
-            116.98180389404297,
-            135.3658447265625,
-            120.45016479492188,
-            123.00967407226562,
-            137.23724365234375,
-            129.23104858398438,
-            131.00619506835938,
-            130.82601928710938,
-            121.90162658691406,
-            120.66400909423828,
-            109.59469604492188,
-            118.66798400878906,
-            126.18386840820312,
-            124.9156494140625,
-            120.90362548828125,
-            106.26658630371094,
-            128.32107543945312,
-            125.82593536376953,
-            121.3394775390625,
-            127.37902069091797,
-            122.92572784423828,
-            126.26400756835938,
-            112.87037658691406,
-            112.48919677734375,
-            126.00082397460938,
-            109.98661804199219,
-            110.7204818725586,
-            107.30191040039062,
-            113.85182189941406,
-            110.24645233154297,
-            133.27935791015625,
-            126.40534973144531,
-            133.42047119140625,
-            112.2728271484375,
-            126.27506256103516,
-            117.58969116210938,
-            119.17208099365234,
-            121.65959167480469,
-            115.62092590332031,
-            118.12762451171875,
-            119.478271484375,
-            137.32974243164062,
-            120.26103210449219,
-            118.25013732910156,
-            121.78120422363281,
-            125.66693115234375,
-            112.0889892578125,
-            115.92691802978516,
-            121.31621551513672,
-            118.76759338378906,
-            126.86924743652344,
-            129.01571655273438,
-            109.53144073486328,
-            110.71353149414062,
-            125.9607925415039,
-            108.36444091796875,
-        ],
-        [
-            114.8194808959961,
-            113.47244262695312,
-            117.47064208984375,
-            115.61190795898438,
-            130.88909912109375,
-            131.42332458496094,
-            120.96844482421875,
-            115.95867919921875,
-            116.98180389404297,
-            135.3658447265625,
-            120.45016479492188,
-            123.00967407226562,
-            137.23724365234375,
-            129.23104858398438,
-            131.00619506835938,
-            130.82601928710938,
-            121.90162658691406,
-            120.66400909423828,
-            109.59469604492188,
-            118.66798400878906,
-            126.18386840820312,
-            124.9156494140625,
-            120.90362548828125,
-            106.26658630371094,
-            128.32107543945312,
-            125.82593536376953,
-            121.3394775390625,
-            127.37902069091797,
-            122.92572784423828,
-            126.26400756835938,
-            112.87037658691406,
-            112.48919677734375,
-            126.00082397460938,
-            109.98661804199219,
-            110.7204818725586,
-            107.30191040039062,
-            113.85182189941406,
-            110.24645233154297,
-            133.27935791015625,
-            126.40534973144531,
-            133.42047119140625,
-            112.2728271484375,
-            126.27506256103516,
-            117.58969116210938,
-            119.17208099365234,
-            121.65959167480469,
-            115.62092590332031,
-            118.12762451171875,
-            119.478271484375,
-            137.32974243164062,
-            120.26103210449219,
-            118.25013732910156,
-            121.78120422363281,
-            125.66693115234375,
-            112.0889892578125,
-            115.92691802978516,
-            121.31621551513672,
-            118.76759338378906,
-            126.86924743652344,
-            129.01571655273438,
-            109.53144073486328,
-            110.71353149414062,
-            125.9607925415039,
-            108.36444091796875,
-        ],
-    ]
-)
-expected_mean_v2 = np.array([120.85137, 120.85137])
-expected_std_v2 = np.array([7.7993703, 7.7993703])
+            [
+                [70.78904458, -2.59054161, -0.50549243],
+                [-2.59054161, 67.21772471, -2.20606105],
+                [-0.50549243, -2.20606105, 73.16562596],
+            ],
+            [
+                [70.66452058, -1.21551709, -1.11634899],
+                [-1.21551709, 67.31813798, -1.05462152],
+                [-1.11634899, -1.05462152, 73.23578343],
+            ],
+        ]
+    )
+}
 
 
-def test_shiftml1_regression():
-    """Regression test for the ShiftML1.1rev model."""
+def test_diamond_regression():
+    """Regression test for ShiftML models."""
 
     frame = bulk("C", "diamond", a=3.566)
-    model = ShiftML("ShiftML1.1rev", force_download=True)
-    out = model.get_cs_iso(frame)
 
-    assert np.allclose(
-        out.flatten(), expected_output
-    ), "ShiftML1 failed regression test"
+    for key, value in expected_outputs.items():
 
+        model = ShiftML(key, force_download=True, device="cpu")
+        out = model.get_cs_iso(frame)
 
-def test_shiftml1_rotational_invariance():
-    """Rotational invariance test for the ShiftML1.1rev model."""
-
-    frame = bulk("C", "diamond", a=3.566)
-    model = ShiftML("ShiftML1.1rev")
-    out = model.get_cs_iso(frame)
-
-    assert np.allclose(
-        out.flatten(), expected_output
-    ), "ShiftML1 failed regression test"
-
-    # Rotate the frame by 90 degrees about the z-axis
-    frame.rotate(90, "z")
-
-    out_rotated = model.get_cs_iso(frame)
-
-    assert np.allclose(
-        out_rotated.flatten(), expected_output
-    ), "ShiftML1 failed rotational invariance test"
+        assert np.allclose(out.flatten(), value), f"{key} failed regression test"
 
 
 def test_shiftml1_size_extensivity_test():
-    """Test ShiftML1.1rev for translational invariance."""
+    """Test ShiftML mdodel for size extensivity"""
 
     frame = bulk("C", "diamond", a=3.566)
-    model = ShiftML("ShiftML1.1rev")
-    out = model.get_cs_iso(frame)
-
-    assert np.allclose(
-        out.flatten(), expected_output
-    ), "ShiftML1 failed regression test"
-
     frame = frame.repeat((2, 1, 1))
-    out = model.get_cs_iso(frame)
+
+    for key, value in expected_outputs.items():
+
+        model = ShiftML(key, device="cpu")
+        out = model.get_cs_iso(frame)
+
+        assert np.allclose(
+            out.flatten(), np.stack([value, value]).flatten()
+        ), f"{key} failed regression test"
+
+
+def test_shiftml3_tensors():
+    frame = bulk("C", "diamond", a=3.566)
+    model = ShiftML("ShiftML3", device="cpu")
+    cs_tensor = model.get_cs_tensor(frame, return_symmetric=True)
+    assert cs_tensor.shape == (2, 3, 3), "CS tensor shape mismatch"
+
+    # assert that the tensor is symmetric
+    assert np.allclose(
+        cs_tensor, cs_tensor.transpose(0, 2, 1)
+    ), "CS tensor is not symmetric"
 
     assert np.allclose(
-        out.flatten(), np.stack([expected_output, expected_output]).flatten()
-    ), "ShiftML1 failed size extensivity test"
+        cs_tensor, expected_outputs_tensors["ShiftML3"], rtol=1e-4
+    ), "CS tensor values do not match expected output"
 
 
-def test_shftml1_fail_invalid_species():
+def test_shftml3_fail_invalid_species():
     """Test ShiftML1.1rev for non-fitted species"""
 
     frame = bulk("Si", "diamond", a=3.566)
-    model = ShiftML("ShiftML1.1rev")
+    model = ShiftML("ShiftML3", device="cpu")
     with pytest.raises(ValueError) as exc_info:
         model.get_cs_iso(frame)
 
@@ -212,25 +83,3 @@ def test_shftml1_fail_invalid_species():
     assert "Model is fitted only for the following atomic numbers:" in str(
         exc_info.value
     )
-
-
-def test_shiftml2_regression_mean():
-    """Regression test for the ShiftML2.0rev model."""
-
-    frame = bulk("C", "diamond", a=3.566)
-    model = ShiftML("ShiftML2.0rev", force_download=True)
-    out_mean = model.get_cs_iso(frame)
-    out_std = model.get_cs_iso_std(frame)
-    out_ensemble = model.get_cs_iso_ensemble(frame)
-
-    assert np.allclose(
-        out_mean.flatten(), expected_mean_v2, atol=0.01
-    ), "ShiftML2 failed regression mean test"
-
-    assert np.allclose(
-        out_std.flatten(), expected_std_v2, atol=0.01
-    ), "ShiftML2 failed regression variance test"
-
-    assert np.allclose(
-        out_ensemble.flatten(), expected_ensemble_v2.flatten(), atol=0.01
-    ), "ShiftML2 failed regression ensemble test"
